d9c6c96bc57bbd3117a9bd7f29bd5813bf78f35b
Aman683054 <47798504+Aman683054@users.noreply.github.com>
2020-05-14 11:58:41 -0400

    Update settings.py

--- mysiteF19/settings.py
+++ mysiteF19/settings.py
@@ -135,8 +135,8 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'myapp/sent_emails')
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'amann663565@gmail.com'
-# EMAIL_HOST_PASSWORD = 'cpizertbpgvgxhgm'
+# EMAIL_HOST_USER = 'abc@gmail.com'
+# EMAIL_HOST_PASSWORD = '***************'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # DEFAULT_FROM_EMAIL = 'CodingWithAman Team <noreply@codingwithaman.com>'
